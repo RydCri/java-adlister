7c1c791e772bff4f61542815eade220215ddee6b
RydCri <>
2023-03-14 11:16:30 -0700

    url patterns

--- src/main/java/AdServlet.java
+++ src/main/java/AdServlet.java
@@ -5,13 +5,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/ads/index")
+@WebServlet(name = "AdServlet", urlPatterns = "/ads")
 public class AdServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        String ads = request.getParameter("ads");
-        request.setAttribute("ads", ads);
         request.setAttribute("ads", DaoFactory.getAdsDao().all());
         request.getRequestDispatcher("/ads/index.jsp").forward(request, response);
     }
