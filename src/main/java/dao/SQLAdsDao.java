b5fbfa868511248ca00ac6ef0df6a64e890d7ad4
RydCri <>
2023-03-16 00:50:38 -0700

    connection is null

--- src/main/java/dao/SQLAdsDao.java
+++ src/main/java/dao/SQLAdsDao.java
@@ -1,20 +1,48 @@
 package dao;
+import com.mysql.cj.jdbc.Driver;
+import model.Ad;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
-import java.sql.DriverManager;
-import java.sql.SQLException;
+public class SQLAdsDao implements Ads {
 
-public class SQLAdsDao {
-          try {
-        DriverManager.registerDriver(new Driver());
-        Config config = new Config();
-        connection = DriverManager.getConnection(
-                config.getUrl(),
-                config.getUser(),
-                config.getPassword()
-        );
-    } catch (
-    SQLException e) {
-        throw new RuntimeException(e);
+    private Connection connection;
+    public SQLAdsDao() {
+
+        try {
+            DriverManager.registerDriver(new Driver());
+            Config config = new Config();
+            Connection connection = DriverManager.getConnection(
+                    config.getUrl(),
+                    config.getUser(),
+                    config.getPassword()
+            );
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
-}
+        @Override
+        public List<Ad> all() {
+            List<Ad> Ads;
+            try {
+                Ads = new ArrayList<>();
+                Statement stmt = connection.createStatement();
+                ResultSet rs = stmt.executeQuery("SELECT * FROM Ads");
+
+                while (rs.next()) {
+                    Ad ad = new Ad(rs.getString("title"), rs.getString("description"));
+                    Ads.add(ad);
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            return Ads;
+        }
+
+    @Override
+    public Long insert(Ad ad) {
+        return null;
+    }
 }
