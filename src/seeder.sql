7d471bc683f6b580239c697e0c13d55839ae2763
RydCri <>
2023-03-19 22:28:06 -0700

    create ad works, added styling

--- src/seeder.sql
+++ src/seeder.sql
@@ -1,7 +1,7 @@
 USE adlister_db;
 
-INSERT INTO users (username, email, password) VALUES
-('jim','jim@jimmail.com','jimothy');
+INSERT INTO users (id,username, email, password) VALUES
+(4,'jim','jim@jimmail.com','jimothy');
 
-INSERT INTO ads (title, description) VALUES
+INSERT INTO ads (user_id,title, description) VALUES
 (1,'New used Couch','Found this couch, new - like used: never been sat upon');
