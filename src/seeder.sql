c8431d75ba45b26330e701072e58beea4bc941f1
RydCri <>
2023-03-19 21:56:34 -0700

    broke create ads page somehow

--- src/seeder.sql
+++ src/seeder.sql
@@ -3,5 +3,5 @@ USE adlister_db;
 INSERT INTO users (username, email, password) VALUES
 ('jim','jim@jimmail.com','jimothy');
 
-INSERT INTO ads (user_id, title, description) VALUES
+INSERT INTO ads (title, description) VALUES
 (1,'New used Couch','Found this couch, new - like used: never been sat upon');
